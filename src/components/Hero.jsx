37cf61fc6b28c6eb4a3aba1708b2fba5e587acb5
Jennifer <jmarthagodfrey@gmail.com>
2025-02-18 13:07:54 -0700

    feat: created info section and base style with tailwind

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 const Hero = () => {
     return (
         <section 
-        className="flex items-center justify-center h-[75vh] bg-[url('./assets/background-image.webp')] bg-cover bg-center bg-no-repeat text-white text-center">
+        className="flex items-center justify-center h-[75vh] bg-[url('./assets/background-image.webp')] bg-cover  bg-no-repeat text-white text-center">
             <div 
             className= "bg-[#0000009a] p-12">
-                <h1 className="p-1 text-3xl uppercase font-bold border-b-1 border-gray-400">Jennifer Godfrey</h1>
-                <p className="p-1 text-xl uppercase">Software Engineer</p>
+                <h1 className="p-1 text-2xl uppercase font-bold border-b-1 border-gray-400">Jennifer Godfrey</h1>
+                <p className="p-1 text-lg uppercase font-semibold">Software Engineer</p>
                 <p className="mt-8">Building with React, Javascript, Node.js, Express, MongoDb</p>
                 <button className="mt-4 py-2 px-12 uppercase font-semibold border-1">Contact Me</button>
             </div>
---
+++ src/components/Info.jsx
@@ -0,0 +1,21 @@
+const Info = () => {
+    return (
+        <section className="my-18 flex flex-col items-center justify-center text-center">
+            <h2 className="uppercase font-semibold text-2xl">A little more about me</h2>
+            <div className="max-w-screen-md mt-8 px-6">
+                <p>
+                    I&apos;m a full-stack software engineer working with JavaScript,
+                    React, Node.js, Express, and MongoDB. Solving real-life problems
+                    with impactful solutions that contribute to a more sustainable
+                    and equitable future.
+                </p>
+                <p className="mt-2">
+                    Let&apos;s connect to explore remote work opportunities and build
+                    something amazing together!
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Info;
