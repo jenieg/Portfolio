dd83a6b418202cc9af8c07d0bda8d403566c4175
Jennifer <jmarthagodfrey@gmail.com>
2025-02-18 13:12:46 -0700

    created project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Hero from './components/Hero'
 import Info from './components/Info'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <main>
       <Hero />
       <Info />
+      <Projects />
     </main>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section>
+        <h2 className='uppercase'>My Favorite Projects</h2>
+    </section>
+  )
+}
+
+export default Projects
